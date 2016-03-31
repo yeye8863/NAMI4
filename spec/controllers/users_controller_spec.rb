34c79c3350fcc4d7e7dd617f1372818092a3e35d
xiaoxiaoxika <zheng100@tamu.edu>
2016-03-31 01:04:16 +0000

    userspec 1

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe UsersController, type: :controller do
     before :each do
         @fake_user = double('user')
         @fake_email = double('email')
-        allow(User).to receive(:save) {true}
+        allow(User).to receive(:save).and_return(true)
         allow(Access).to receive(:exists?).with('email') {true} 
     end
     
@@ -22,37 +22,37 @@ RSpec.describe UsersController, type: :controller do
         
         
         it 'checks the access of the new user' do
-            
+            skip
         end
         #happy path: this new user has access to signup
         it 'saves user data' do
-        
+          pending
         end
         
         it 'turn to login page' do
-        
+            pending
         end
         
         it 'sets a notice saying "You have signed up successfully."' do
-        
+         pending
         end
         
         #sad path: this new user does not have access to signup
         it 'stays on signup page' do
-            
+            pending
         end
         
         it 'sets a notice saying "Incorrect access code."' do
-            
+            pending
         end
         
         #sad path: this new user enters different password when confirmation
         it 'stays on signup page' do
-        
+            pending
         end
         
         it 'sets a notice saying "Error occured."' do
-            
+            pending
         end
         
     end
