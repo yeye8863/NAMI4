0f62a67bfe4c8a932ef18496cc92b45574c06912
Lihao Zou <loe.zou@gmail.com>
2016-04-07 22:33:12 +0000

    style1

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,8 +2,7 @@ require 'date'
 
 class DashboardsController < ApplicationController
     before_filter :authorize
-
     def index
-      @agenda_records = Contact.find_agenda
+      @agenda_records = AgendaView.all
     end
 end
--- db/migrate/20160407034500_create_agenda_view.rb
+++ db/migrate/20160407034500_create_agenda_view.rb
@@ -1,12 +1,13 @@
 class CreateAgendaView < ActiveRecord::Migration
-  def change
+  def up
     create_view :agenda_views, 
       %Q(
         SELECT * FROM (SELECT 
           a.title||' '||a.first_name||' '||a.last_name as name,
           NULL as organization,
           c.contact_date as contact_date,
-          c.followup_date as followup_date
+          c.followup_date as followup_date,
+          c.id as contact_id
         FROM donors a JOIN contacts c ON a.id = c.donor_id
         WHERE c.followup_date >= date('now')
         UNION
@@ -14,11 +15,16 @@ class CreateAgendaView < ActiveRecord::Migration
           b.title||' '||b.first_name||' '||b.last_name as name,
           d.name as organization,
           c.contact_date as contact_date,
-          c.followup_date as followup_date
+          c.followup_date as followup_date,
+          c.id as contact_id
         FROM contacts c JOIN contact_people b ON c.contact_person_id = b.id
         JOIN organizations d ON b.organization_id = d.id
         WHERE c.followup_date >= date('now'))
         ORDER BY followup_date ASC
         ) 
+  end
+  
+  def down
+    drop_view :agenda_views, :if_exists => true
   end
 end
