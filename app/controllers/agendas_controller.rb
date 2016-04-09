4c6b46005e11c53c400df38be7ec2d1983de6a74
Lihao Zou <loe.zou@gmail.com>
2016-04-09 02:07:47 +0000

    agenda feature 2

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,2 +1,7 @@
 class AgendasController < ApplicationController
+  def show
+    id = params[:id]
+    @contact = Contact.find(id)
+    render(:partial => 'agenda_details', :object => @contact) if request.xhr?
+  end
 end
