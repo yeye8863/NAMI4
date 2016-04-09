3173c9bbfcc60adc8b237c78c4dcf5c4b2bc8d4c
nypatrician <huichangdanteng@yahoo.com>
2016-04-09 02:12:09 +0000

    user summary

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -8,8 +8,29 @@ class DonorsController < ApplicationController
     def new 
     end
     
-    def summary
+    def show
         id = params[:id]
         @donor = Donor.find(id)
+        @donor_attr = {
+            'Title' => @donor.title,
+            'First Name' => @donor.first_name,
+            'Last Name' => @donor.last_name,
+            'Middle Name' => @donor.middle_name,
+            'Salution' => @donor.salution,
+            'Email' => @donor.email,
+	        'Organization' => @donor.organization,
+	        'Company' => @donor.company,
+	        'Street Address' => @donor.street_address,
+	        'City' => @donor.city,
+	        'State' => @donor.state,
+	        'Countrt' => @donor.country,
+	        'Zip Code' => @donor.zipcode,
+            'Home Phone' => @donor.home_phone,
+            'Business Phone' => @donor.business_phone,
+            'Created By' => @donor.created_by,
+            'Last Modified by' => @donor.last_modified_by,
+            'Created at' => @donor.created_at,
+            'Last Modified at' => @donor.last_modified_at
+	        }
     end
 end
