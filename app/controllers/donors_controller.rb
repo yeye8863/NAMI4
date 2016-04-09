b7b7f814993383a20cfd7268d2d17ecccb2da62f
TonyWang <tonybestrong@gmail.com>
2016-04-09 00:14:59 +0000

    updated donor search view

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -2,9 +2,16 @@ class DonorsController < ApplicationController
     #before_filter :authorize
 
     def index
-        @donors = Donor.where(params[:donor])
+        @donors = Donor.search_by(params[:donor])
     end
     
     def new 
     end
+    
+    def create
+        @donor = Donor.create!(params[:donor])
+        flash[:notice] = "#{@donor.first_name} #{@donor.last_name}  was successfully created."
+        redirect_to donors_path
+    end
 end
+
--- app/models/donor.rb
+++ app/models/donor.rb
@@ -2,5 +2,16 @@ class Donor < ActiveRecord::Base
     attr_accessible :first_name, :last_name, :company, :organization
     has_many :finances
     has_many :contacts
+    
+    def self.search_by inputs
+        if inputs != nil 
+           inputs.delete_if {|key, value| value.empty? }
+           inputs.each do |key, value|
+               value.capitalize! 
+           end
+        end
+        
+        return Donor.where(inputs)
+    end
 
 end
