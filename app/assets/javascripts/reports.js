addd0775b050eccf0b8e2a5fc74f6704e35290fe
misu <houjiayin@tamu.edu>
2016-04-30 21:48:58 +0000

    fixing

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,1 +1,12 @@
 //$("#report_records").html("<%= escape_javascript(render("reports")) %>");
+var viewResult = {
+  setup : function() {
+    $('#search_result th a, #search_result .pagination a').click(viewResult.render);
+  },
+  render : function(){
+    $(document).on('ajax:success',function(event,data,status,xhrObj){
+      $('#search_result').html(data);
+    });
+  }
+};
+$(document).ready(viewResult.setup);
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( filter.js )
 
 
-Rails.application.config.assets.precompile += %w(filter.js)
+Rails.application.config.assets.precompile += %w(filter.js reports.js)
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
