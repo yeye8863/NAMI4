fdc703ebff0ea299104ff88ce08935caa2880bc0
stephen2526 <yuanfei25fairy@gmail.com>
2016-05-02 05:48:10 +0000

    before mer with repo

--- db/schema.rb
+++ db/schema.rb
@@ -129,6 +129,7 @@ SELECT * FROM
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "current_password"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "function"
