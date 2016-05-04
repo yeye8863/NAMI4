e5ba930e25135bf6b453bf623fd7d748260b57f4
Lihao Zou <loe.zou@gmail.com>
2016-05-04 03:20:53 +0000

    fix user search

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,19 +2,20 @@ var userTable={
   setup: function(){
     var table = $('#user_table').DataTable();
     
+    // append column search box
     $('#user_table tfoot th.filter').each(function(){
         var title = $(this).text();
         $(this).html( '<input type="text" placeholder=" '+title+'" />' );
     });
     
-    // Apply the search
+    // apply column search
     table.columns().every( function () {
         var that = this;
         $( 'input', this.footer() ).on( 'keyup change', function () {
             if ( that.search() !== this.value ) {
                 that
                     .search( this.value)
-                    .draw();
+                    .draw(false);
             }
         } );
     } );
