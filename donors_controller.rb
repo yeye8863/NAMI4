df06fffc2173a9c6cc0671a77ae7d2c1ba1618a6
GordonCC94 <gchen940215@gmail.com>
2016-10-25 18:47:40 -0500

    Modification
    Added subscribe field,
    Modified page formatting

---
+++ donors_controller.rb
@@ -0,0 +1,330 @@
+require 'csv'
+
+class DonorsController < ApplicationController
+
+    before_action :check_authorization
+
+
+    def index
+        @donor_attr = Donor.attribute_names
+        @donor_attr_show = ["flag", "title", "first_name", "last_name", "organization", "company"]
+        @donors = Donor.search_by(params[:donor]).where('active = 1')
+    end
+
+    def new
+        @donor = Donor.new
+        @donor_contact = [
+	        "contact_date",
+	        "followup_date",
+	        "narrative"
+	    ]
+	    @donor_finance = [
+            '_type',
+            'date',
+            'amount',
+            'description',
+            'designation'
+        ]
+        #render(:partial => 'donor_info', :object => @donor) if request.xhr?
+    end
+
+    def destroy
+        id = params[:id]
+        @donor = Donor.find(id)
+        flash[:notice] = "#{@donor.first_name} #{@donor.last_name} is deleted."
+        @donor.update_attributes(:active => 0)
+        redirect_to donors_path
+    end
+
+    def create
+        params[:donor][:active] = 1
+        @donor = Donor.create!(params[:donor])
+        #flash[:notice] = "#{@donor.first_name} #{@donor.last_name} was successfully created."
+        if params[:where] == "inplace"
+            render :json => @donor
+        else
+            render :json => {:id => @donor.id}
+        end
+    end
+
+    def show
+        id = params[:id]
+        @active = params[:active]
+        @donor = Donor.find(id)
+
+
+      if @donor.subscribeflag == "Y"
+        subscribe = "Yes"
+      elsif @donor.subscribeflag == "N"
+        subscribe = "No"
+      end
+
+      render :json => @donor if request.xhr? && params[:where] == "inplace"
+      @donor_basic = {
+            'Subscribe' => subscribe,
+            'Title' => @donor.title,
+            'Role' => @donor.role,
+            'First Name' => @donor.first_name,
+            'Last Name' => @donor.last_name,
+            'Middle Name' => @donor.middle_name,
+            'Salution' => @donor.salution,
+            'Email' => @donor.email,
+  	        'Organization' => @donor.organization,
+  	        'Company' => @donor.company,
+  	        'Spouse' => @donor.spouse,
+  	        'Street Address' => @donor.street_address,
+  	        'Street Address 2' => @donor.street_address_2,
+  	        'City' => @donor.city,
+  	        'State' => @donor.state,
+  	        'Countrt' => @donor.country,
+  	        'Zip Code' => @donor.zipcode,
+            'Cell Phone' => @donor.cell_phone,
+            'Home Phone' => @donor.home_phone,
+            'Business Phone' => @donor.business_phone,
+            'Note' => @donor.note
+            
+	        }
+	    @donor_contact = [
+	        "contact_date",
+	        "followup_date",
+	        "narrative"
+	    ]
+	    @contacts = Contact.where(:donor_id => @donor.id)
+	    @finances = Finance.where(:donor => @donor.id)
+	    @donor_finance = [
+            '_type',
+            'date',
+            'amount',
+            'description',
+            'designation'
+        ]
+    end
+
+    def update
+      @donor = Donor.find(params[:id])
+      @donor.update_attributes(params[:donor])
+
+      if @donor.flag == "I"
+        type = "Individual"
+      elsif @donor.flag == "O"
+        type = "Organization"
+      end
+
+
+      if @donor.subscribeflag == "Y"
+        subscribe = "Yes"
+      elsif @donor.subscribeflag == "N"
+        subscribe = "No"
+      end
+
+      render :json => @donor if request.xhr? && params[:where] == "inplace"
+      @donor_basic = {
+            'Subscribe' => subscribe,
+            'Title' => @donor.title,
+            'Role' => @donor.role,
+            'First Name' => @donor.first_name,
+            'Last Name' => @donor.last_name,
+            'Middle Name' => @donor.middle_name,
+            'Salution' => @donor.salution,
+            'Email' => @donor.email,
+  	        'Organization' => @donor.organization,
+  	        'Company' => @donor.company,
+  	        'Spouse' => @donor.spouse,
+  	        'Street Address' => @donor.street_address,
+  	        'Street Address 2' => @donor.street_address_2,
+  	        'City' => @donor.city,
+  	        'State' => @donor.state,
+  	        'Country' => @donor.country,
+  	        'Zip Code' => @donor.zipcode,
+            'Cell Phone' => @donor.cell_phone,
+            'Home Phone' => @donor.home_phone,
+            'Business Phone' => @donor.business_phone,
+            'Note' => @donor.note
+            
+	        }
+      render(:partial => 'donor_summary', :object => @donor_basic) if request.xhr? && !params[:where]
+    end
+
+    def showByContact
+      contact_id = params[:contactId]
+      @contact = Contact.find(contact_id)
+      @donor = @contact.donor
+      redirect_to controller:'donors', action:'show', id:@donor.id, active:2
+    end
+
+    def showSummary
+      id = params[:id]
+      @donor = Donor.find(id)
+
+      if @donor.flag == "I"
+        type = "Individual"
+      elsif @donor.flag == "O"
+        type = "Organization"
+      end
+
+     if @donor.subscribeflag == "Y"
+        subscribe = "Yes"
+      elsif @donor.subscribeflag == "N"
+        subscribe = "No"
+      end
+
+      @donor_basic = {
+          'Subscribe' => subscribe,
+          'Title' => @donor.title,
+          'Role' => @donor.role,
+          'First Name' => @donor.first_name,
+          'Last Name' => @donor.last_name,
+          'Middle Name' => @donor.middle_name,
+          'Salution' => @donor.salution,
+          'Email' => @donor.email,
+  	      'Organization' => @donor.organization,
+  	      'Company' => @donor.company,
+  	      'Spouse' => @donor.spouse,
+  	      'Street Address' => @donor.street_address,
+  	      'Street Address 2' => @donor.street_address_2,
+  	      'City' => @donor.city,
+  	      'State' => @donor.state,
+  	      'Country' => @donor.country,
+  	      'Zip Code' => @donor.zipcode,
+          'Cell Phone' => @donor.cell_phone,
+          'Home Phone' => @donor.home_phone,
+          'Business Phone' => @donor.business_phone,
+          'Note' => @donor.note
+          
+	     }
+	     render(:partial => 'donor_summary', :object => @donor_basic) if request.xhr?
+    end
+
+    def importIndex
+      session[:upload_path]=nil
+    end
+
+    def upload
+      @uploaded_file = params[:post][:file]
+      session[:upload_path] = @uploaded_file.path
+      session[:file_type] = File.extname(@uploaded_file.original_filename)
+      render :json => 'Successfully Uploaded' if request.xhr?
+    end
+
+    def import
+      response_array={}
+      if(session[:upload_path])
+        case session[:file_type]
+        when ".csv" then @file = Roo::CSV.new(session[:upload_path])
+        when ".xls" then @file = Roo::Excel.new(session[:upload_path])
+        when ".xlsx" then @file = Roo::Excelx.new(session[:upload_path])
+        end
+        @file.default_sheet = @file.sheets.first
+      end
+
+      if(!@file)
+        response_array['status']=1;
+        render :json=>response_array if request.xhr?
+        return
+      end
+
+      @models = []
+      @fields = []
+      @donor_index = []
+      @contact_index = []
+      @finance_index = []
+
+      isEmpty = (params[:selector]["0"].join=="")?true:false
+
+      if(isEmpty)
+        response_array['status']=2;
+        render :json=>response_array if request.xhr?
+        return
+      end
+
+      counter = 0
+      if(!isEmpty)
+        params[:selector].each_value do |row|
+          @models << row[0].classify.constantize
+          @fields << row[1].parameterize.underscore.to_sym
+          if (row[0]=="Donor")
+            @donor_index << counter
+          elsif (row[0]=="Contact")
+            @contact_index << counter
+          elsif (row[0]=="Finance")
+            @finance_index << counter
+          end
+          counter+=1
+        end
+      end
+
+      donor_param = {}
+      contact_param = {}
+      finance_param = {}
+
+      row_stop = @file.last_row
+      (2...row_stop).each do |line_num|
+        @donor_index.each do |d_i|
+          donor_param[@fields[d_i]] = @file.row(line_num)[d_i]
+        end
+        @contact_index.each do |c_i|
+          if(@fields[c_i]==:contact_date || @fields[c_i]==:followup_date)
+            contact_param[@fields[c_i]] = Date.strptime(@file.row(line_num)[c_i],"%m/%d/%Y")
+          else
+            contact_param[@fields[c_i]] = @file.row(line_num)[c_i]
+          end
+        end
+        @finance_index.each do |f_i|
+          if(@fields[f_i]==:date )
+            finance_param[@fields[f_i]] = Date.strptime(@file.row(line_num)[f_i],"%m/%d/%Y")
+          elsif(@fields[f_i]==:amount)
+            finance_param[@fields[f_i]] = @file.row(line_num)[f_i].to_f
+          else
+            finance_param[@fields[f_i]] = @file.row(line_num)[f_i]
+          end
+        end
+
+        donor_param[:active] = 1
+        donor_param[:created_by] = session[:user]
+        donor_param[:last_modified_by] = session[:user]
+        donor_param[:last_modified_at] = DateTime.now
+        @donor = Donor.new donor_param
+        if !@donor.save
+          #debugger
+          response_array['status'] = 3
+          render :json=>response_array if request.xhr?
+          return
+        end
+
+        if !contact_param.empty?
+          contact_param[:created_by] = session[:user]
+          contact_param[:last_modified_by] = session[:user]
+          contact_param[:last_modified_at] = DateTime.now
+          @contact = @donor.contacts.new contact_param
+          if !@contact.save
+            #debugger
+            response_array['status'] = 3
+            render :json=>response_array if request.xhr?
+            return
+          end
+        end
+
+        if !finance_param.empty?
+          finance_param[:created_by] = session[:user]
+          finance_param[:last_modified_by] = session[:user]
+          finance_param[:last_modified_at] = DateTime.now
+          @finance = @donor.finances.new finance_param
+          if !@finance.save
+            #debugger
+            response_array['status'] = 3
+            render :json=>response_array if request.xhr?
+            return
+          end
+        end
+      end
+      response_array['status']=0
+      render :json => response_array if request.xhr?
+    end
+
+    def check_authorization
+        unless current_user.function and current_user.function.include? 'donor management'
+            flash[:notice]="Sorry, authorization check failed!"
+            redirect_to homepage_path
+        end
+    end
+end
---
+++ schema.rb
@@ -0,0 +1,155 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20161025223434) do
+
+  create_table "accesses", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", :null=>false
+    t.datetime "updated_at", :null=>false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.date     "contact_date"
+    t.date     "followup_date"
+    t.text     "narrative"
+    t.integer  "donor_id"
+    t.string   "created_by"
+    t.datetime "created_at",       :null=>false
+    t.string   "last_modified_by"
+    t.datetime "last_modified_at"
+    t.datetime "updated_at",       :null=>false
+  end
+  add_index "contacts", ["donor_id"], :name=>"index_contacts_on_donor_id"
+
+  create_table "donors", force: :cascade do |t|
+    t.string   "title"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "middle_name"
+    t.string   "salution"
+    t.string   "email"
+    t.string   "organization"
+    t.string   "company"
+    t.string   "street_address"
+    t.string   "city"
+    t.string   "state",            :default=>"Texas"
+    t.string   "country",          :default=>"United States"
+    t.string   "zipcode"
+    t.string   "home_phone"
+    t.string   "business_phone"
+    t.string   "created_by"
+    t.string   "last_modified_by"
+    t.datetime "created_at",       :null=>false
+    t.datetime "last_modified_at"
+    t.datetime "updated_at",       :null=>false
+    t.string   "flag"
+    t.integer  "active",           :default=>1
+    t.string   "role"
+    t.string   "note"
+    t.string   "spouse"
+    t.string   "street_address_2"
+    t.string   "cell_phone"
+    t.string   "subscribeflag"
+  end
+
+  create_view "agenda_views", <<-'END_VIEW_AGENDA_VIEWS', :force => true
+SELECT * FROM 
+      (
+        SELECT 
+          a.title||' '||a.first_name||' '||a.last_name as name,
+          a.organization as organization,
+          c.contact_date as contact_date,
+          c.followup_date as followup_date,
+          c.id as contact_id
+        FROM donors a JOIN contacts c ON a.id = c.donor_id
+        WHERE c.followup_date >= date('now') AND a.active = 1) as RESULT
+        ORDER BY followup_date ASC
+  END_VIEW_AGENDA_VIEWS
+
+  create_table "filters", force: :cascade do |t|
+    t.string   "table_name"
+    t.string   "field_name"
+    t.string   "value"
+    t.decimal  "min_value"
+    t.decimal  "max_value"
+    t.date     "min_date"
+    t.date     "max_date"
+    t.string   "created_by"
+    t.datetime "created_at",       :null=>false
+    t.string   "last_modified_by"
+    t.datetime "last_modified_at"
+    t.integer  "report_id"
+    t.datetime "updated_at",       :null=>false
+  end
+  add_index "filters", ["report_id"], :name=>"index_filters_on_report_id"
+
+  create_table "finances", force: :cascade do |t|
+    t.string   "_type"
+    t.date     "date"
+    t.decimal  "amount"
+    t.text     "description"
+    t.string   "designation"
+    t.integer  "donor_id"
+    t.integer  "contact_id"
+    t.string   "created_by"
+    t.datetime "created_at",       :null=>false
+    t.string   "last_modified_by"
+    t.datetime "last_modified_at"
+    t.datetime "updated_at",       :null=>false
+  end
+  add_index "finances", ["contact_id"], :name=>"index_finances_on_contact_id"
+  add_index "finances", ["donor_id"], :name=>"index_finances_on_donor_id"
+
+  create_table "functions", force: :cascade do |t|
+    t.string   "name"
+    t.string   "created_by"
+    t.string   "last_modified_by"
+    t.datetime "created_at",       :null=>false
+    t.datetime "last_modified_at"
+    t.datetime "updated_at",       :null=>false
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "last_run"
+    t.string   "created_by"
+    t.datetime "created_at",       :null=>false
+    t.string   "last_modified_by"
+    t.datetime "last_modified_at"
+    t.datetime "updated_at",       :null=>false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "current_password"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "function"
+    t.string   "created_by"
+    t.string   "last_modified_by"
+    t.datetime "created_at",       :null=>false
+    t.datetime "last_modified_at"
+    t.datetime "updated_at",       :null=>false
+    t.string   "phone_number"
+    t.string   "street_address"
+    t.string   "city"
+    t.string   "state",            :default=>"Texas"
+    t.string   "country",          :default=>"United States"
+    t.integer  "zipcode"
+  end
+
+end
